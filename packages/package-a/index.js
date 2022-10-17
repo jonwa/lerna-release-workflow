5a8d84e1821b908d20a7560bd0c5b444e4967673
Luis Felipe <luisfmsouza@gmail.com>
2022-10-17 13:52:08 -0300

    chore: remove feature from package A

--- packages/package-a/index.js
+++ packages/package-a/index.js
@@ -5,5 +5,5 @@ module.exports = mainPackage;
 function mainPackage() {
   console.log("Main Package");
   console.log("add new feature");
-  console.log("add another feature");
+  console.log("remove another feature");
 }
